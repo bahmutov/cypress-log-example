9c7ee5dc6ddcbfeda74aaf34f04f2014d3a21a4b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-07-10 08:48:07 -0400

    readme

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # cypress-log-example
 > Example repo for blog post "Why cy.log prints nothing"
 
+The problem
+
+![Logs null](./images/null.png)
+
+The fixed test prints the expected result
+
+![Logs expected text](./images/mary.png)
+
+See the tests in [cypress/integration/spec.js](./cypress/integration/spec.js)
+
 ## More information
 
 - [Cypress tips and tricks](https://glebbahmutov.com/blog/cypress-tips-and-tricks/)
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,9 +1,51 @@
 /// <reference types="cypress" />
 
-it('prints the text', () => {
+it('prints null', () => {
   cy.visit('index.html')
   // ⛔️ INCORRECT - PRINTS NULL
   let username = null
   cy.get('#username').then(($el) => (username = $el.text()))
   cy.log(username) // always prints null ⚠️
+})
+
+it('prints null with event trace', () => {
+  cy.on('command:enqueued', ({ name, args }) => {
+    console.log(`ENQUEUED ${name}: ${args}`)
+  })
+  cy.on('command:start', ({ attributes: { name, args } }) => {
+    console.log(`START ${name}: ${args}`)
+  })
+  cy.visit('index.html')
+  // ⛔️ INCORRECT - PRINTS NULL
+  let username = null
+  cy.get('#username').then(($el) => (username = $el.text()))
+  cy.log(username) // always prints null ⚠️
+})
+
+it('prints the text', () => {
+  cy.visit('index.html')
+  // ✅ CORRECT - prints "Mary"
+  let username = null
+  cy.get('#username')
+    .then(($el) => (username = $el.text()))
+    .then(() => {
+      // by this point the "username" primitive variable
+      // has been set, and the call is made cy.log("Mary")
+      cy.log(username)
+    })
+})
+
+it('prints text with event trace', () => {
+  cy.on('command:enqueued', ({ name, args }) => {
+    console.log(`ENQUEUED ${name}: ${args}`)
+  })
+  cy.on('command:start', ({ attributes: { name, args } }) => {
+    console.log(`START ${name}: ${args}`)
+  })
+  cy.visit('index.html')
+  // ✅ CORRECT - prints "Mary"
+  cy.get('#username')
+    .invoke('text')
+    // avoid printing internals of cy.log in the trace
+    .then((s) => cy.log(s))
 })
